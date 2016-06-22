64e35d88277f965c49d404e759a22670db5232f5
Dan Beam <dbeam@chromium.org>
2016-06-22 15:13:42 -0700

    account for border

--- outer-dimensions.js
+++ outer-dimensions.js
@@ -1,18 +1,66 @@
+(function() {
+
+// Note: box-sizing: border-box; doesn't seem to affect results as
+// offset{Height,Width} already accounts for this.
+
+/**
+ * @param {!HTMLElement} el
+ * @param {!CSSStyleDeclaration} style
+ */
+function getHeight(el, style) {
+  return el.offsetHeight
+      // These are accounted for by offsetWidth.
+      // + parseFloat(style.borderTopWidth)
+      // + parseFloat(style.borderBottomWidth)
+      + parseFloat(style.marginTop)
+      + parseFloat(style.marginBottom);
+}
+
+/**
+ * @param {!HTMLElement} el
+ * @param {!CSSStyleDeclaration} style
+ */
+function getWidth(el, style) {
+  return el.offsetWidth
+      // + parseFloat(style.borderLeftWidth)
+      // + parseFloat(style.borderRightWidth)
+      + parseFloat(style.marginLeft)
+      + parseFloat(style.marginRight);
+}
+
+/**
+ * @param {!HTMLElement} el
+ * @return {boolean} Whether |el| can have valid dimensions.
+ */
+function hasDimensions(el) {
+  return !!el.parentNode;
+}
+
 Object.defineProperties(HTMLElement.prototype, {
-  outerHeight: {
+  outerDimensions: {
     get: function() {
-      if (!this.parentNode) return 0;
-      var style = getComputedStyle(this);
-      // Note: box-sizing: border-box; doesn't affect results as offsetHeight accounts for this.
-      return this.offsetHeight + parseFloat(style.marginTop) + parseFloat(style.marginBottom);
+      if (!hasDimensions(this))
+        return {height: 0, width: 0};
+
+      var styles = getComputedStyle(this);
+      return {
+        height: getHeight(this, styles),
+        width: getWidth(this, styles),
+      };
     },
   },
+
+  outerHeight: {
+    get: function() {
+      return hasDimensions(this) ? getHeight(this, getComputedStyle(this)) : 0;
+    },
+  },
+
   outerWidth: {
     get: function() {
-      if (!this.parentNode) return 0;
-      var style = getComputedStyle(this);
-      // Note: box-sizing: border-box; doesn't affect this either.
-      return this.offsetWidth + parseFloat(style.marginLeft) + parseFloat(style.marginRight);
+      return hasDimensions(this) ? getWidth(this, getComputedStyle(this)) : 0;
     },
   },
 });
+
+})();
